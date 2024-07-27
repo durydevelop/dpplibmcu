d23eed92593215bff0499d2f160ec03e94f88b4a
fabio <fabio@photorec.it>
2024-07-27 19:08:18 +0200

    sab 27 lug 2024, 19:08:18, CEST

---
+++ TODO.md
@@ -0,0 +1,7 @@
+TODO:
+- [ ] DI2cMaster::askForString()
+
+DONE:
+0.2.0
+- [x] i2c examples
+    - [x] i2c-ask
--- src/dgpio/dgpio.cpp
+++ src/dgpio/dgpio.cpp
@@ -3,77 +3,70 @@
 
 /**
  * @brief Initilize gpio chip.
- * - Must be call once before any gpio operation.
- * - InitPin() call it by it self
- * - On ARDUINO return true.
+ * - Need to be called once.
+ * - initPin() can call it by itself.
  * 
  * @param gpioDevice    ->  device index.
- * @param DGpioHandle   ->  handle from initGpio(), on ARDUINO is ignored.
- * @return true on success otherwise false.
+ * @param handle        ->  (not for Arduino) the handle obtained from initGpio() or DGpioChip class.
+ * @return DRES_OK on success otherwise a DResult error code (you can call getErrorCode(result) for retriving error string).
+ * On ARDUINO return always DRES_OK.
  */
-DResult initGpio(int gpioDevice, DGpioHandle& handle)
+#ifdef ARDUINO
+DResult initGpio(int gpioDevice)
 {
-    #ifdef ARDUINO
-        return DRES_OK;
-    #elif defined LGPIO_VERSION
-        if (handle < 0) {
-            handle = lgGpiochipOpen(gpioDevice);
-            if (handle < 0) {
-                fatal("Cannot continue: lgGpiochipOpen() failed with error: ",handle);
-            }
-            return handle;
-        }
-    #elif defined PGPIO_VERSION
-        if (gpioInitialise() == PI_INIT_FAILED) {
-            return false;
-        }
-    #endif
     return DRES_OK;
 }
-
+#else
+DResult initGpio(int gpioDevice, DGpioHandle& handle)
+{
+    handle = lgGpiochipOpen(gpioDevice);
+    if (handle < 0) {
+        fatal("Cannot continue: lgGpiochipOpen() failed with error: ",handle);
+    }
+    return handle;
+}
+#endif
 
 /**
  * @brief Check if gpio chip is initilized.
- * On ARDUINO return true;
+ * On ARDUINO return always true;
  * 
- * @param DGpioHandle   ->  handle from initGpio(), on ARDUINO is ignored.
+ * @param handle    ->  (not for Arduino) the handle obtained from initGpio() or DGpioChip class.
  * @return true on success otherwise false.
  */
-bool isGpioReady(DGpioHandle& handle)
+#ifdef ARDUINO
+bool isGpioReady(void)
 {
-    #ifdef ARDUINO
-        return true;
-    #elif defined LGPIO_VERSION
-        if (handle < 0) {
-            return false;
-        }
-        lgChipInfo_t cInfo;
-        int status = lgGpioGetChipInfo(0, &cInfo);
-        if (status == LG_OKAY) {
-            return true;
-        }
-        return false;
-    #elif defined PGPIO_VERSION
-        if (gpioInitialise() == PI_INITIALISED) {
-            return true;
-        }
-        return false;
-    #endif
+    return true;
 }
+#else
+bool isGpioReady(DGpioHandle handle)
+{
+    if (handle < 0) {
+        return false;
+    }
+    lgChipInfo_t cInfo;
+    int status = lgGpioGetChipInfo(0, &cInfo);
+    if (status == LG_OKAY) {
+        return true;
+    }
+    return false;
+}
+#endif
 
 
 /**
  * @brief Configures a pin to be used as input, output or whatever.
  * 
- * @param pin           ->  gpio pin.
- * @param mode          ->  one of DPinMode values.
- * @param flags         ->  on of DPinFlags values.
- * @param DGpioHandle   ->  handle from initGpio(), on ARDUINO is ignored.
- * @return true on success otherwise false.
+ * @param pin       ->  gpio pin.
+ * @param mode      ->  one of DPinMode values.
+ * @param flags     ->  on of DPinFlags values.
+ * @param handle    ->  (not for Arduino) the handle obtained from initGpio() or DGpioChip class.
+ * @return DRES_OK on success otherwise a DResult error code (you can call getErrorCode(result) for retriving error string).
  */
-DResult initPin(uint8_t pin, DPinMode mode, DPinFlags flags, DGpioHandle handle)
+#ifdef ARDUINO
+DResult initPin(uint8_t pin, DPinMode mode, DPinFlags flags)
 {
-    #ifdef ARDUINO
         if (Mode == DPinMode::SOFT_PWM) {
             #ifdef USE_DSOFT_PWM
                 /// @todo dPwmOut=new DPwmOut(Pin);
@@ -85,7 +78,10 @@ DResult initPin(uint8_t pin, DPinMode mode, DPinFlags flags, DGpioHandle handle)
             pinMode(Pin,Mode);
         }
         return DRES_OK;
-    #elif defined LGPIO_VERSION
+}
+#else
+DResult initPin(uint8_t pin, DPinMode mode, DPinFlags flags, DGpioHandle handle)
+{
         if (handle < 0) {
             // Try init on first device
             initGpio(0,handle);
@@ -108,58 +104,48 @@ DResult initPin(uint8_t pin, DPinMode mode, DPinFlags flags, DGpioHandle handle)
                 break;
         }
         return ret;
-    #elif defined PIGPIO_VERSION
-        // TODO: deprecate
-        if (Mode == OUTPUT_PWM_MODE) {
-            gpioSetMode(Pin,PI_OUTPUT);
-        }
-        else {
-            gpioSetMode(Pin,Mode);
-        }
-        return DRES_OK;
-    #endif
 }
+#endif
 
 /**
  * @brief Read pin level.
  * 
- * @param Pin           -> gpio pin.
- * @param DGpioHandle   ->  handle from initGpio(), on ARDUINO is ignored.
+ * @param Pin       -> gpio pin.
+ * @param handle    ->  (not for Arduino) the handle obtained from initGpio() or DGpioChip class.
  * @return pin level HIGH or LOW (negative number is errCode)
  */
+#ifdef ARDUINO
+int readPin(uint8_t pin)
+{
+    return digitalRead(pin);
+}
+#else
 int readPin(uint8_t pin, DGpioHandle handle)
 {
-    #ifdef ARDUINO
-        return digitalRead(pin);
-    #elif defined LGPIO_VERSION
-        return lgGpioRead(handle,pin);
-    #elif defined PIGPIO_VERSION
-        // TODO: deprecate
-        return gpioRead(Pin);
-    #endif
+    return lgGpioRead(handle,pin);
 }
+#endif
 
 /**
  * @brief Set pin level.
  * 
- * @param pin           ->  gpio pin.
- * @param level         ->  level: HOGH or LOW.
- * @param DGpioHandle   ->  handle from initGpio(), on ARDUINO is ignored.
- * @return true on success otherwise false.
+ * @param pin       ->  gpio pin.
+ * @param level     ->  level: HOGH or LOW.
+ * @param handle    ->  (not for Arduino) the handle obtained from initGpio() or DGpioChip class.
+ * @return DRES_OK on success otherwise a DResult error code (you can call getErrorCode(result) for retriving error string).
  */
+#ifdef ARDUINO
+DResult writePin(uint8_t pin, uint8_t level)
+{
+    digitalWrite(Pin,Level);
+    return DRES_OK;
+}
+#else
 DResult writePin(uint8_t pin, uint8_t level, DGpioHandle handle)
 {
-    #ifdef ARDUINO
-		digitalWrite(Pin,Level);
-        return DRES_OK
-    #elif defined LGPIO_VERSION
-        return lgGpioWrite(handle,pin,level);
-    #elif defined PIGPIO_VERSION
-        // TODO: deprecate
-        gpioWrite(Pin,Level);
-        return DRES_OK;
-    #endif
+    return lgGpioWrite(handle,pin,level);
 }
+#endif
 
 /**
  * @brief Output a pwm signal to pin.
@@ -168,61 +154,59 @@ DResult writePin(uint8_t pin, uint8_t level, DGpioHandle handle)
  * PWM output only works on the pins with hardware support.
  * See https://www.arduino.cc/reference/en/language/functions/analog-io/analogwrite/ for details.
  * 
- * @param pin           ->  gpio pin.
- * @param Value         ->  pwm value from 0 to 255.
- * @param DGpioHandle   ->  handle from initGpio(), on ARDUINO is ignored.
- * @return true on success otherwise false.
+ * @param pin       ->  gpio pin.
+ * @param Value     ->  pwm value from 0 to 255.
+ * @param handle    ->  (not for Arduino) the handle obtained from initGpio() or DGpioChip class.
+ * @return DRES_OK on success otherwise a DResult error code (you can call getErrorCode(result) for retriving error string).
  */
+#ifdef ARDUINO
+DResult writeAnalog(uint8_t pin, uint8_t value)
+{
+    analogWrite(pin,value);
+    return DRES_OK;
+}
+#else
 DResult writeAnalog(uint8_t pin, uint8_t value, DGpioHandle handle)
 {
-    #ifdef ARDUINO
-        analogWrite(pin,value);
-        return DRES_OK;
-    #elif defined LGPIO_VERSION
-        float dutyCycle=mapValue(value,0,255,0,100);
-        return lgTxPwm(handle,pin,500,dutyCycle,0,0);
-    #elif defined PIGPIO_VERSION
-        // TODO: deprecate
-        gpioPWM(Pin,Value);
-        return DRES_OK;
-    #endif
+    float dutyCycle=mapValue(value,0,255,0,100);
+    return lgTxPwm(handle,pin,500,dutyCycle,0,0);
 }
+#endif
 
 /**
  * @brief Free a pin that can be claimed for an other use.
- * On ARDUINO return true;
  * 
- * @param pin           ->  gpio pin.
- * @param DGpioHandle   ->  handle from initGpio(), on ARDUINO is ignored.
- * @return true on success otherwise false.
+ * @param pin       ->  gpio pin.
+ * @param handle    ->  (not for Arduino) the handle obtained from initGpio() or DGpioChip class.
+ * @return DRES_OK on success otherwise a DResult error code (you can call getErrorCode(result) for retriving error string).
  */
+#ifdef ARDUINO
+DResult releasePin(uint8_t pin)
+{
+    return DRES_OK;
+}
+#else
 DResult releasePin(uint8_t pin, DGpioHandle handle)
 {
-    #ifdef ARDUINO
-        return DRES_OK;
-    #elif defined LGPIO_VERSION
-        return lgGpioFree(handle,pin);
-    #elif defined PIGPIO_VERSION
-        return DRES_OK;
-    #endif
+    return lgGpioFree(handle,pin);
 }
+#endif
 
 /**
  * @brief Close handle for gpio chip.
  * On ARDUINO return true;
  * 
- * @param DGpioHandle   ->  handle from initGpio(), on ARDUINO is ignored.
- * @return true on success otherwise false.
+ * @param handle    ->  (not for Arduino) the handle obtained from initGpio() or DGpioChip class.
+ * @return DRES_OK on success otherwise a DResult error code (you can call getErrorCode(result) for retriving error string).
  */
-
+#ifdef ARDUINO
+DResult shutdownGpio(void)
+{
+    return DRES_OK;
+}
+#else
 DResult shutdownGpio(DGpioHandle handle)
 {
-    #ifdef ARDUINO
-        return DRES_OK;
-    #elif defined LGPIO_VERSION
-        return lgGpiochipClose(handle);
-    #elif defined PGPIO_VERSION
-        gpioTerminate();
-        return DRES_OK;
-    #endif
+    return lgGpiochipClose(handle);
 }
+#endif
